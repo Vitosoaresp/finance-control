dfa30b3b405925034858d54f78717ff21baf4b6b
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-23 21:38:31 -0300

    feat: dashBoard

---
+++ src/components/Dashboard/CardDashboard.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from "react";
+
+interface ICardDashboadProps {
+  icon: ReactElement;
+  type: 'Entradas' | 'Saídas' | 'Total';
+  value: number;
+}
+
+export function CardDashboard(props: ICardDashboadProps) {
+  return (
+    <div
+      className={`${props.type === 'Total' ? 'bg-emerald-800' : 'bg-[#323238]'} flex flex-col gap-3 w-[352px] p-6 rounded-md ${props.type === 'Total' && 'border'} border-black`}
+    >
+
+      <div className="flex items-center w-full justify-between">
+        <span className="font-normal text-gray-400">{props.type}</span>
+        <span>{props.icon}</span>
+      </div>
+
+      <div className="w-full font-bold text-3xl text-white">
+        <span>{props.value.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}</span>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/components/Dashboard/Dasboard.tsx
@@ -0,0 +1,24 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { CardDashboard } from "./CardDashboard";
+
+export function Dashboard() {
+  return (
+    <div className="flex gap-8 justify-center absolute -top-20 lef-1/2 right-1/2 translate-x-1/2">
+      <CardDashboard
+        type="Entradas"
+        value={27000}
+        icon={<ArrowCircleUp color="#00B37E" size={32} />}
+      />
+      <CardDashboard
+        type="Saídas"
+        value={25000}
+        icon={<ArrowCircleDown color="#F75A68" size={32} />}
+      />
+      <CardDashboard
+        type="Total"
+        value={27000}
+        icon={<CurrencyDollar color="white" size={32} />}
+      />
+    </div>
+  )
+}
