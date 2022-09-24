df1a97f68b8b88c6e789b5e9db231f2d3a93eecb
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-23 22:24:27 -0300

    table fake

---
+++ src/TableLines.ts
@@ -0,0 +1,32 @@
+export const TableLines = [
+  {
+    descricao: 'Desenvolvimento web',
+    preco: 12000,
+    categoria: 'Salário',
+    data: new Date('09/23/2022'),
+  },
+  {
+    descricao: 'Pizza',
+    preco: -89,
+    categoria: 'Comida',
+    data: new Date('09/20/2022'),
+  },
+  {
+    descricao: 'Academia',
+    preco: -150,
+    categoria: 'Lazer',
+    data: new Date('09/10/2022'),
+  },
+  {
+    descricao: 'Desenvolvimento web',
+    preco: 5000,
+    categoria: 'Freelancer',
+    data: new Date('09/05/2022'),
+  },
+  {
+    descricao: 'Lanche',
+    preco: -39,
+    categoria: 'Comida',
+    data: new Date('08/30/2022'),
+  },
+];
