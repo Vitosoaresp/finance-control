f4216a1fcbe42df41f3375c70447c9d557cba51f
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-23 21:38:08 -0300

    feat: create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Money } from 'phosphor-react';
+
+export default function Header() {
+  return (
+    <header className="bg-black pt-10 px-40 pb-32">
+      <div className="flex h-12 justify-between w-full ">
+        <p className="flex items-center gap-2 text-2xl font-bold text-zinc-200">
+          <Money color="#015F43" size={72} />
+          <span>Finance Control</span>
+        </p>
+
+        <button
+          className="text-base font-bold py-3 px-5 bg-emerald-700 rounded hover:bg-emerald-800 transition-colors text-white"
+        >
+          Nova transação
+        </button>
+      </div>
+    </header>
+  )
+}
