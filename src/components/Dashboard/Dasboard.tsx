5c38e1321d10366a595a056ea492d3e3ff332a2f
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-25 00:12:05 -0300

    refac: get state data to dashboard

--- src/components/Dashboard/Dasboard.tsx
+++ src/components/Dashboard/Dasboard.tsx
@@ -1,22 +1,36 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { ITransaction } from '../../App';
 import { CardDashboard } from './CardDashboard';
 
-export function Dashboard() {
+interface IDashboadProps {
+  transactions: ITransaction[];
+}
+
+export function Dashboard(props: IDashboadProps) {
+
+  const incoming = props.transactions
+    .filter(transaction => transaction.option === 'Entrada')
+    .reduce((acc, current) => acc += Number(current.price), 0);
+    
+  const expense = props.transactions
+    .filter(transaction => transaction.option === 'Saída')
+    .reduce((acc, current) => acc += Number(current.price), 0);
+
   return (
     <div className="flex gap-8 justify-center absolute -top-20 lef-1/2 right-1/2 translate-x-1/2">
       <CardDashboard
         type="Entradas"
-        value={27000}
+        value={incoming}
         icon={<ArrowCircleUp color="#00B37E" size={32} />}
       />
       <CardDashboard
         type="Saídas"
-        value={25000}
+        value={expense}
         icon={<ArrowCircleDown color="#F75A68" size={32} />}
       />
       <CardDashboard
         type="Total"
-        value={27000}
+        value={incoming - expense}
         icon={<CurrencyDollar color="white" size={32} />}
       />
     </div>
